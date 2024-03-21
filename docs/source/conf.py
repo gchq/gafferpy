843526bd827c542d171cdb769531d4781dbbf1c0
tb06904 <141412860+tb06904@users.noreply.github.com>
2024-03-21 14:28:32 +0000

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,6 @@ author = 'GCHQ'
 # The full version, including alpha/beta/rc tags
 release = '2.2.0'
 
-
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
