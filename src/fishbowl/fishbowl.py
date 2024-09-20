gchq/gafferpy

commit a94ccaeabca4e7925ee89e826e3e056806bb2b05

Gh-35: Refactor tests to use tox and pytest (#36)
* Refactor test suite to use tox and pytest, and have separate directories for unit and integration tests. Update READMEs, and fishbowl scripts and jinja templates. Refactor fishbowl to generate import strings.

* Copyright and licence

src/fishbowl/fishbowl.py
@@ -1,5 +1,5 @@
 #
-# Copyright 2022 Crown Copyright
+# Copyright 2023 Crown Copyright
 #
 # Licensed under the Apache License, Version 2.0 (the 'License');
 # you may not use this file except in compliance with the License.
@@ -14,30 +14,40 @@
 # limitations under the License.
 #
 
-import os
+from pathlib import Path
 import re
 import shutil
 from jinja2 import Environment, FileSystemLoader
+from gafferpy.gaffer_connector import GafferConnector
 from gafferpy.gaffer_core import JsonConverter
 from gafferpy.gaffer_types import parse_java_type_to_string
+from collections import defaultdict
+from typing import Any, Callable, Dict, List, Set
 
 
 class Fishbowl:
-    def __init__(self, gaffer_connector, generated_directory_path="generated"):
+    def __init__(
+        self,
+        gaffer_connector: GafferConnector,
+        generated_directory_path: str = "generated"
+    ):
         self._gaffer_connector = gaffer_connector
         self.generated_directory_path = generated_directory_path
-        print("Generating python Library from REST service")
+        print("Generating Python API from REST service...")
         self._generate_library()
-        print("done")
-        print("To import operations, predicates and functions, use the following command:")
-        print("from " + generated_directory_path + " import *")
+        print(
+            "Successfully generated Python API! To import operations, predicates and "
+            "functions, use the following command: "
+            f"\n`from {str(generated_directory_path)} import *`"
+        )
 
-    def _write_to_file(self, file_path, data):
+    def _write_to_file(self, file_path: Path, data: str) -> None:
         if data:
+            file_path.unlink()  # removes file
             with open(file_path, "w+") as file:
                 file.write(data)
 
-    def _get_config_name(self, url):
+    def _get_config_name(self, url: str) -> str:
         name = url.replace("/graph", "").replace("/config", "")
         # Remove parameter
         name = re.sub(r"\/\{.*\}", "", name)
@@ -46,20 +56,24 @@ class Fishbowl:
         name = "Get" + "".join([name[0].upper() + name[1:] for name in name.split("/")])
         return name
 
-    def _get_function_dict(self):
+    def _get_function_dict(self) -> Dict[str, Callable]:
         return {
             "short_name": lambda n: n.rsplit(".", 1)[1].replace("$", ""),
             "snake_case": JsonConverter.to_snake_case,
             "get_fields": lambda f: self._gaffer_connector.get(
-                f"/graph/config/serialisedFields/{f}", json_result=True),
+                f"/graph/config/serialisedFields/{f}", json_result=True
+            ),
+            "get_field_types": lambda f: self._gaffer_connector.get(
+                f"/graph/config/serialisedFields/{f}/classes", json_result=True
+            ),
             "config_name": self._get_config_name,
             "get_config_parameter": lambda n: re.sub(r".*\{", "", re.sub(r"\}.*", "", n)),
             "parse_java_type": parse_java_type_to_string
         }
 
     def _generate_library(self):
-        parent_dir = os.path.dirname(__file__)
-        templates_dir = os.path.join(parent_dir, "templates")
+        parent_dir = Path(__file__).parent
+        templates_dir = parent_dir / "templates"
         file_loader = FileSystemLoader(templates_dir)
         self.env = Environment(loader=file_loader)
 
@@ -69,46 +83,60 @@ class Fishbowl:
         binary_operators_python = self._generate_aggregation_functions()
         config_python = self._generate_config()
 
-        if os.path.exists(self.generated_directory_path):
-            shutil.rmtree(self.generated_directory_path)
-
-        os.mkdir(self.generated_directory_path)
+        if not self.generated_directory_path.exists():
+            self.generated_directory_path.mkdir()
 
         self._write_to_file(
-            os.path.join(self.generated_directory_path, "functions.py"), functions_python)
+            self.generated_directory_path / "functions.py",
+            functions_python
+        )
         self._write_to_file(
-            os.path.join(self.generated_directory_path, "predicates.py"), predicates_python)
+            self.generated_directory_path / "predicates.py",
+            predicates_python
+        )
         self._write_to_file(
-            os.path.join(
-                self.generated_directory_path, "binary_operators.py"), binary_operators_python)
+            self.generated_directory_path / "binaryoperators.py",
+            binary_operators_python
+        )
         self._write_to_file(
-            os.path.join(self.generated_directory_path, "operations.py"), operations_python)
+            self.generated_directory_path / "config.py",
+            config_python
+        )
         self._write_to_file(
-            os.path.join(self.generated_directory_path, "config.py"), config_python)
+            self.generated_directory_path / "operations.py",
+            operations_python
+        )
         self._write_to_file(
-            os.path.join(self.generated_directory_path, "__init__.py"),
-            "__all__ = [\"operations\", \"predicates\", \"functions\", \"binary_operators\", \"config\"]\n")
+            self.generated_directory_path / "__init__.py",
+            "__all__ = [\"operations\", \"predicates\", \"functions\", "
+            "\"binary_operators\", \"config\"]\n"
+        )
 
-    def _generate_transform_functions(self):
+    def _generate_transform_functions(self) -> str:
         return self._generate_functions(
             "transformFunctions",
-            "gaffer_functions",
-            "AbstractFunction")
+            "AbstractFunction"
+        )
 
-    def _generate_filter_functions(self):
+    def _generate_filter_functions(self) -> str:
         return self._generate_functions(
             "filterFunctions",
-            "gaffer_predicates",
-            "AbstractPredicate")
+            "AbstractPredicate"
+        )
 
-    def _generate_aggregation_functions(self):
+    def _generate_aggregation_functions(self) -> str:
         return self._generate_functions(
             "aggregationFunctions",
-            "gaffer_binaryoperators",
-            "AbstractBinaryOperator")
+            "AbstractBinaryOperator"
+        )
 
-    def _generate_functions(self, path, import_path, base_class):
-        # Older Gaffer versions may be missing some function end points
+    def _generate_functions(
+        self,
+        path: str,
+        base_class: str,
+        base_class_import_path: str = "gafferpy.gaffer_core"
+    ) -> str:
+        # Older Gaffer versions may be missing some function endpoints
         try:
             functions = self._gaffer_connector.get(f"/graph/config/{path}", json_result=True)
         except ConnectionError:
@@ -116,15 +144,19 @@ class Fishbowl:
             return ""
 
         functions = sorted(functions)
+        manual_imports = {base_class_import_path: {base_class}}
+        imports = self._generate_import_strings(functions, manual_imports)
 
         function_template = self.env.get_template("functions.py.j2")
         function_template.globals.update(self._get_function_dict())
         return function_template.render(
             functions=functions,
-            import_path=import_path,
-            base_class=base_class)
+            import_path=base_class_import_path,
+            base_class=base_class,
+            imports=imports
+        )
 
-    def _generate_operations(self):
+    def _generate_operations(self) -> str:
         # spring-rest has an endpoint for every store operation, even ones unsupported by the store
         # Check if this is supported: 2.0.0+, 1.23.0+
         try:
@@ -140,11 +172,18 @@ class Fishbowl:
 
         operation_summaries = sorted(operation_summaries, key=lambda op: op["name"])
 
+        op_names = [op['name'] for op in operation_summaries]
+
+        imports = self._generate_import_strings(op_names)
+
         template = self.env.get_template("operations.py.j2")
         template.globals.update(self._get_function_dict())
-        return template.render(operations=operation_summaries)
+        return template.render(
+            operations=operation_summaries,
+            imports=imports
+        )
 
-    def _generate_config(self):
+    def _generate_config(self) -> str:
         # Gaffer 2 spring-rest uses openapi3 rather than swagger2
         try:
             api_summaries = self._gaffer_connector.get("/v3/api-docs", json_result=True)
@@ -155,14 +194,60 @@ class Fishbowl:
                 print(f"swagger information not present, skipping config")
                 return ""
 
-        configs = []
-        for path, data in api_summaries["paths"].items():
-            if path.startswith("/graph/") and "get" in data:
-                configs.append(path)
+        configs = [
+            path for path, data in api_summaries["paths"].items()
+            if path.startswith("/graph/") and "get" in data
+        ]
 
         template = self.env.get_template("config.py.j2")
         template.globals.update(self._get_function_dict())
         return template.render(configs=configs)
+
+    def _generate_import_strings(self,
+                                 funcs: List[Any],
+                                 manual_imports: Dict[str,
+                                                      Set] = dict()) -> List[str]:
+        """
+        To stop circular import issues during generation, we only want to import the specific class names we need.
+        I.e. rather than `from gafferpy.gaffer_binaroperators import *`, we want
+        `from gafferpy.gaffer_binaryoperators import BinaryOperator, ...`
+
+        Args:
+            funcs: List of Gaffer Functions, Predicates or BinaryOperators for which to generate import strings.
+            manual_imports: Dictionary of manual imports to add, where the keys are manual import paths and values are
+                the class to import.
+
+        Returns:
+            List of import strings e.g. `from gafferpy.gaffer_binaryoperators import BinaryOperator`
+
+        """
+        get_field_types = self._get_function_dict()['get_field_types']
+
+        class_names = [func.split(".")[-1] for func in funcs]
+
+        import_map = defaultdict(set)
+        #  add the manual imports first
+        import_map.update(manual_imports)
+
+        for func in funcs:
+            for java_type in get_field_types(func).values():
+                python_type = parse_java_type_to_string(java_type)
+                if 'gafferpy' not in python_type:
+                    continue
+
+                gafferpy_import = re.findall("gafferpy[\\w+ \\.]*", python_type)[0]
+                gafferpy_import = gafferpy_import.replace("generated_api.", "gaffer_")
+                import_parts = gafferpy_import.split('.')
+                module = ".".join(import_parts[:-1])
+                _class = import_parts[-1]
+                # only import classes not already defined in the file in question
+                if _class not in class_names:
+                    import_map[module].add(_class)
+
+        return [
+            f"from {import_path} import {', '.join(sorted(classes))}" for import_path,
+            classes in import_map.items()
+        ]
 
     def get_connector(self):
         return self._gaffer_connector

.github/workflows/continuous-integration.yaml
@@ -50,12 +50,14 @@ jobs:
         curl -o jetty-runner.jar https://repo1.maven.org/maven2/org/eclipse/jetty/jetty-runner/9.4.52.v20230823/jetty-runner-9.4.52.v20230823.jar
         curl -o road-traffic-rest.war https://repo1.maven.org/maven2/uk/gov/gchq/gaffer/road-traffic-rest/$gafferVersion/road-traffic-rest-$gafferVersion.war
         java \
-        -Dgaffer.storeProperties=src/test/road-traffic-example/store.properties \
-        -Dgaffer.graph.config=src/test/road-traffic-example/graphConfig.json \
-        -Dgaffer.schemas=src/test/road-traffic-example/schema \
-        -DroadTraffic.dataLoader.dataPath=src/test/road-traffic-example/roadTrafficSampleData.csv \
+        -Dgaffer.storeProperties=tests/road-traffic-example/store.properties \
+        -Dgaffer.graph.config=tests/road-traffic-example/graphConfig.json \
+        -Dgaffer.schemas=tests/road-traffic-example/schema \
+        -DroadTraffic.dataLoader.dataPath=tests/road-traffic-example/roadTrafficSampleData.csv \
         -jar jetty-runner.jar --path rest road-traffic-rest.war &
         sleep 1m
 
     - name: Run Python tests
-      run: python3 -m unittest discover -s src
+      run: |
+        python3 -m pip install ".[dev]"
+        python3 -m tox

.github/workflows/update-gaffer-version.yaml
@@ -39,7 +39,7 @@ jobs:
       run: |
         newVersion=${{ github.event.inputs.version }}
         curl -o spring-rest.jar https://repo.maven.apache.org/maven2/uk/gov/gchq/gaffer/spring-rest/$newVersion/spring-rest-$newVersion-exec.jar
-        java -Dgaffer.schemas=src/test/road-traffic-example/schema -Dgaffer.storeProperties=src/test/road-traffic-example/federatedStore.properties -Dgaffer.graph.config=src/test/road-traffic-example/federatedGraphConfig.json -jar spring-rest.jar &
+        java -Dgaffer.schemas=tests/road-traffic-example/schema -Dgaffer.storeProperties=tests/road-traffic-example/federatedStore.properties -Dgaffer.graph.config=tests/road-traffic-example/federatedGraphConfig.json -jar spring-rest.jar &
         sleep 1m
         python src/generate.py
         rm spring-rest.jar

.gitignore
@@ -94,7 +94,7 @@ celerybeat.pid
 
 # Environments
 .env
-.venv
+.venv*
 .venv-python-example
 env/
 venv/

setup.py
@@ -1,13 +1,13 @@
 import re
 from codecs import open
-from os import path
+from pathlib import Path
 from setuptools import setup, find_packages
 
 ###############################################################################
 
 name = "gafferpy"
 packages = find_packages(where="src")
-meta_path = path.join("src", "gafferpy", "__init__.py")
+meta_path = Path("src", "gafferpy", "__init__.py")
 keywords = ["class", "attribute", "boilerplate"]
 classifiers = [
     "Development Status :: 4 - Beta",
@@ -24,20 +24,27 @@ classifiers = [
 python_requires = ">3.6"
 install_requires = []
 extras_require = {
-    "requests": ["requests>=2.4.0"]
+    "requests": ["requests>=2.4.0"],
+    "dev": [
+        "tox",
+        "pytest",
+        "requests>=2.4.0",
+        "sphinx~=7.2.6",
+        "sphinx-rtd-theme~=1.3.0"
+    ]
 }
 
 ###############################################################################
 
-here = path.abspath(path.dirname(__file__))
+here = Path(__file__).parent
 
 
-def read(*parts):
+def read(parts):
     """
     Build an absolute path from *parts* and and return the contents of the
     resulting file.  Assume UTF-8 encoding.
     """
-    with open(path.join(here, *parts), "rb", "utf-8") as f:
+    with open(here / parts, "rb", "utf-8") as f:
         return f.read()
 
 
@@ -54,14 +61,14 @@ def find_meta(meta):
     )
     if meta_match:
         return meta_match.group(1)
-    raise RuntimeError("Unable to find __{meta}__ string.".format(meta=meta))
+    raise RuntimeError(f"Unable to find __{meta}__ string.")
 
 
 version = find_meta("version")
 uri = find_meta("uri")
 
 # Get the long description from the README.md file
-with open(path.join(here, "README.md"), encoding="utf-8") as f:
+with open(here / "README.md", encoding="utf-8") as f:
     long = f.read()
 
 setup(

src/fishbowl/templates/config.py.j2
@@ -12,10 +12,9 @@ class {{ config_name(conf) }}(GetGraph):
     def __init__(self):
     {%- endif %}
     {%- if '{' in conf %}
-        super().__init__(
-            '{{ conf }}'.format({{ get_config_parameter(conf) }}={{ snake_case(get_config_parameter(conf)) }}))
+        super().__init__(f"{{ conf | replace(get_config_parameter(conf), snake_case(get_config_parameter(conf))) }}")
     {%- else %}
-        super().__init__('{{ conf }}')
+        super().__init__("{{ conf }}")
     {%- endif %}
 
 {%- endfor %}

src/fishbowl/templates/functions.py.j2
@@ -1,9 +1,15 @@
 {% extends 'base.py.j2' %}
 
 {% block content %}
-from gafferpy.{{ import_path }} import {{ base_class }}
+import typing
+
+{%- for import in imports %}
+{{ import }}
+{%- endfor %}
+
 {%- for func in functions %}
 {%- set fields = get_fields(func) %}
+{%- set field_types_dict = get_field_types(func) %}
 
 
 class {{ short_name(func) }}({{ base_class }}):
@@ -11,11 +17,11 @@ class {{ short_name(func) }}({{ base_class }}):
     {%- if fields|length > 0 %}
 
     def __init__(
-            self,
-            {%- for field in (fields[:-1]) %}
-            {{ snake_case(field) }}=None,
-            {%- endfor %}
-            {{ snake_case(fields[-1]) }}=None):
+        self,
+        {%- for field in (fields) %}
+        {{ snake_case(field) }}: typing.Optional[{{ parse_java_type(field_types_dict.get(field), False) }}] = None,
+        {%- endfor %}
+    ):
     {%- else %}
 
     def __init__(self):

src/fishbowl/templates/operations.py.j2
@@ -3,9 +3,9 @@
 {% block content %}
 import typing
 
-import gafferpy.gaffer_core
-import gafferpy.gaffer_functions
-from gafferpy.gaffer_operations import Operation, View, Conditional, AggregatePair
+{%- for import in imports %}
+{{ import }}
+{%- endfor %}
 {% for op in operations %}
 {%- if op.name != "uk.gov.gchq.gaffer.operation.OperationChainDAO" %}
 
@@ -26,13 +26,15 @@ class {{ short_name(op.name) }}(Operation):
     CLASS = "{{ op.name }}"
 
     def __init__(
-            self,
-            {%- for field in op.fields|sort(reverse=true, attribute="required") %}
-            {%- if field.name != "options" %}
-            {{ snake_case(field.name) }}: {{ parse_java_type(field.className) }}{% if not field.required %} = None{% endif %},
-            {%- endif %}
-            {%- endfor %}
-            options: typing.Dict[str, str] = None):
+        self,
+        {%- for field in op.fields|sort(reverse=true, attribute="required") %}
+        {%- if field.name != "options" %}
+        {{ snake_case(field.name) }}: {% if field.required %}{{ parse_java_type(field.className, False) }}{% else %}typing.Optional[{{ parse_java_type(field.className, False) }}] = None{% endif %},
+        {%- endif %}
+        {%- endfor %}
+        options: typing.Optional[typing.Dict[str, str]] = None
+    ):
+
         super().__init__(_class_name=self.CLASS, options=options)
         {%- for field in op.fields %}
         {%- if field.name != "options" %}

commit f8757e771dc23e176f943c09cf1c3da142027dc3

Fix running fishbowl without a Path argument and existing files Fixes a bug introduced in #36 which broke the generation example given in the README

src/fishbowl/fishbowl.py
@@ -30,10 +30,10 @@ class Fishbowl:
     def __init__(
         self,
         gaffer_connector: GafferConnector,
-        generated_directory_path: str = "generated"
+        generated_directory_path: str | Path = "generated"
     ):
         self._gaffer_connector = gaffer_connector
-        self.generated_directory_path = generated_directory_path
+        self.generated_directory_path = Path(generated_directory_path)
         print("Generating Python API from REST service...")
         self._generate_library()
         print(
@@ -44,7 +44,8 @@ class Fishbowl:
 
     def _write_to_file(self, file_path: Path, data: str) -> None:
         if data:
-            file_path.unlink()  # removes file
+            if file_path.exists():
+                file_path.unlink()  # removes file
             with open(file_path, "w+") as file:
                 file.write(data)
 